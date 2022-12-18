23ec47105825d5de8504170802a8b285bd855f07
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-18 23:37:41 +0200

    feat(demo-app): use @chill-viking/layout in demo app

--- apps/chill-viking-ng-libs/src/app/app.component.spec.ts
+++ apps/chill-viking-ng-libs/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { ChillVikingLayoutComponent } from '@chill-viking/layout';
+import { MockComponent } from 'ng-mocks';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, MockComponent(ChillVikingLayoutComponent)],
     }).compileComponents();
   });
 
