7d09147303cec756990ce17796f1fc8572728563
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-18 23:37:41 +0200

    feat(demo-app): use @chill-viking/layout in demo app

--- apps/chill-viking-ng-libs/src/app/app.module.ts
+++ apps/chill-viking-ng-libs/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChillVikingLayoutModule } from '@chill-viking/layout';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ChillVikingLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
