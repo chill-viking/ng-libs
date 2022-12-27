b55988248ab88ebbeb688992ff249c0998f0a2b6
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-27 23:34:35 +0200

    feat(@chill-viking/layout): include `footer` in LayoutContext
    Related to #35

--- libs/layout/src/lib/models/layout-context.ts
+++ libs/layout/src/lib/models/layout-context.ts
@@ -1,5 +1,7 @@
+import { LayoutFooterContext } from './layout-footer-context';
 import { LayoutHeaderContext } from './layout-header-context';
 
 export type LayoutContext = {
   header: LayoutHeaderContext;
+  footer?: LayoutFooterContext;
 };
