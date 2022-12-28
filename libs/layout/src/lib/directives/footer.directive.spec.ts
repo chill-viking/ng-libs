9852bee2434522e261a8b1619d8bf4b9f189ed73
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-28 02:29:45 +0200

    feat(@chill-viking/layout): improve directive specs
    Related to #35

--- libs/layout/src/lib/directives/footer.directive.spec.ts
+++ libs/layout/src/lib/directives/footer.directive.spec.ts
@@ -1,70 +1,33 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ContentChild,
-  TemplateRef,
-} from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { LayoutFooterContext } from '@chill-viking/layout';
+import { Injector } from '@angular/core';
+import { FooterDirectiveTemplateContext } from '@chill-viking/layout';
+import { MockService } from 'ng-mocks';
 import { FooterDirective } from './footer.directive';
 
-@Component({
-  selector: 'cv-layout-footer-renderer',
-  template: `
-    <div id="template-render-block">
-      <ng-container
-        [ngTemplateOutlet]="template"
-        [ngTemplateOutletContext]="{ $implicit: ctx }"
-      ></ng-container>
-    </div>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-class LayoutFooterRendererComponent {
-  @ContentChild(FooterDirective, { read: TemplateRef<LayoutFooterContext> })
-  template!: TemplateRef<LayoutFooterContext>;
-  ctx: LayoutFooterContext = {
-    copyrightHolder: 'a person',
-  };
-}
-
-@Component({
-  template: ` <cv-layout-footer-renderer>
-    <ng-template cvFooter let-footer>
-      <p>{{ footer.copyrightHolder }}</p>
-    </ng-template>
-  </cv-layout-footer-renderer>`,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-class HostComponent {}
-
 describe('FooterDirective', () => {
-  let fixture: ComponentFixture<HostComponent>;
-  let component: HostComponent;
+  let mockedDirective: FooterDirective;
+  let context: FooterDirectiveTemplateContext;
+  let directive: FooterDirective;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        FooterDirective,
-        HostComponent,
-        LayoutFooterRendererComponent,
-      ],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(HostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockedDirective = MockService(FooterDirective, {
+      createEmbeddedView: jest.fn(),
+    });
+    context = { $implicit: {} };
+    directive = new FooterDirective(mockedDirective);
   });
 
   it('should create', () => {
-    // if component can be created, then directive also created.
-    expect(component).toBeTruthy();
+    expect(directive).toBeTruthy();
   });
 
-  it('should be rendered correctly in component', () => {
-    const element = fixture.nativeElement;
-    expect(element.querySelector('#template-render-block').innerHTML).toContain(
-      '<p>a person</p>',
+  it('should use injected TemplateRef', () => {
+    const injector = MockService(Injector);
+
+    directive.createEmbeddedView(context, injector);
+
+    expect(mockedDirective.createEmbeddedView).toHaveBeenCalledWith(
+      context,
+      injector,
     );
   });
 });
