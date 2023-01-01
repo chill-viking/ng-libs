5a910211bfe992a83987881a541165a6222bb9f2
Peter Jokumsen <pjokumsen@gmail.com>
2023-01-01 17:14:50 +0200

    feat(demo-app): update pages/home

--- apps/chill-viking-ng-libs/src/app/pages/home/home.component.spec.ts
+++ apps/chill-viking-ng-libs/src/app/pages/home/home.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
+import { ChillVikingHeaderComponent } from '../../features/chill-viking-header/chill-viking-header.component';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +10,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, MockComponent(ChillVikingHeaderComponent)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
--- apps/chill-viking-ng-libs/src/app/pages/home/home.component.ts
+++ apps/chill-viking-ng-libs/src/app/pages/home/home.component.ts
@@ -6,11 +6,12 @@ import {
 } from '@angular/core';
 import { ChillVikingLayoutModule, LayoutContext } from '@chill-viking/layout';
 import { of } from 'rxjs';
+import { ChillVikingHeaderComponent } from '../../features/chill-viking-header/chill-viking-header.component';
 
 @Component({
   selector: 'ng-libs-home',
   standalone: true,
-  imports: [CommonModule, ChillVikingLayoutModule],
+  imports: [CommonModule, ChillVikingLayoutModule, ChillVikingHeaderComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -19,7 +20,11 @@ import { of } from 'rxjs';
 export class HomeComponent {
   context: LayoutContext = {
     header: {
-      title$: of('Chill Viking | ng-libs demos'),
+      title$: of('Chill Viking | ng-libs repository'),
     },
   };
+
+  subTitle$ = of(
+    'Elevate Your Angular Development with Our Handy npm Packages',
+  );
 }
