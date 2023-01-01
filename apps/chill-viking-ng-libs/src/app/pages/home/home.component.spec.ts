e5ce3207582c19176dd51cbd4c6c8cba33635cac
Peter Jokumsen <pjokumsen@gmail.com>
2023-01-01 20:25:16 +0200

    feat(demo-app): update wording for home

--- apps/chill-viking-ng-libs/src/app/pages/home/home.component.spec.ts
+++ apps/chill-viking-ng-libs/src/app/pages/home/home.component.spec.ts
@@ -1,24 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockComponent } from 'ng-mocks';
+import { MockComponent, MockProvider } from 'ng-mocks';
+import { ChillVikingCallToActionComponent } from '../../features/chill-viking-call-to-action/chill-viking-call-to-action.component';
 import { ChillVikingHeaderComponent } from '../../features/chill-viking-header/chill-viking-header.component';
+import { WindowRouterService } from '../../window-router.service';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: WindowRouterService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent, MockComponent(ChillVikingHeaderComponent)],
+      imports: [
+        HomeComponent,
+        MockComponent(ChillVikingHeaderComponent),
+        MockComponent(ChillVikingCallToActionComponent),
+      ],
+      providers: [MockProvider(WindowRouterService)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(WindowRouterService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('goToPackages', () => {
+    it('should navigate to /packages', () => {
+      router.navigate = jest.fn();
+
+      component.goToPackages();
+
+      expect(router.navigate).toHaveBeenCalledWith(['packages']);
+    });
+  });
+
+  describe('goToRepository', () => {
+    it('should open new window to repository', () => {
+      router.open = jest.fn();
+
+      component.goToRepository();
+
+      expect(router.open).toHaveBeenCalledWith(
+        'https://github.com/chill-viking/ng-libs',
+      );
+    });
+  });
+
+  describe('goToDiscussions', () => {
+    it('should open new window to discussions', () => {
+      router.open = jest.fn();
+
+      component.goToDiscussions();
+
+      expect(router.open).toHaveBeenCalledWith(
+        'https://github.com/orgs/chill-viking/discussions',
+      );
+    });
   });
 });
--- apps/chill-viking-ng-libs/src/app/pages/home/home.component.ts
+++ apps/chill-viking-ng-libs/src/app/pages/home/home.component.ts
@@ -4,14 +4,23 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { ChillVikingLayoutModule, LayoutContext } from '@chill-viking/layout';
 import { of } from 'rxjs';
+import { ChillVikingCallToActionComponent } from '../../features/chill-viking-call-to-action/chill-viking-call-to-action.component';
 import { ChillVikingHeaderComponent } from '../../features/chill-viking-header/chill-viking-header.component';
+import { WindowRouterService } from '../../window-router.service';
 
 @Component({
   selector: 'ng-libs-home',
   standalone: true,
-  imports: [CommonModule, ChillVikingLayoutModule, ChillVikingHeaderComponent],
+  imports: [
+    CommonModule,
+    ChillVikingLayoutModule,
+    ChillVikingHeaderComponent,
+    ChillVikingCallToActionComponent,
+    MatIconModule,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -20,11 +29,25 @@ import { ChillVikingHeaderComponent } from '../../features/chill-viking-header/c
 export class HomeComponent {
   context: LayoutContext = {
     header: {
-      title$: of('Chill Viking | ng-libs repository'),
+      title$: of('Chill Viking | ng-libs'),
     },
   };
 
   subTitle$ = of(
     'Elevate Your Angular Development with Our Handy npm Packages',
   );
+
+  constructor(private _router: WindowRouterService) {}
+
+  goToPackages(): void {
+    this._router.navigate(['packages']);
+  }
+
+  goToRepository(): void {
+    this._router.open('https://github.com/chill-viking/ng-libs');
+  }
+
+  goToDiscussions(): void {
+    this._router.open('https://github.com/orgs/chill-viking/discussions');
+  }
 }
