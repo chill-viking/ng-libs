0777ca62c26eddb916d0739b921368168e4e5073
Peter Jokumsen <pjokumsen@gmail.com>
2023-01-05 08:55:59 +0200

    feat(demo-app): set title using TitleStrategy

--- apps/chill-viking-ng-libs/src/main.ts
+++ apps/chill-viking-ng-libs/src/main.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
+import { PageTitleStrategyService } from './app/page-title-strategy.service';
 import { RootComponent } from './app/root.component';
 import { rootRouterFeatures, rootRoutes } from './app/routes';
 
 bootstrapApplication(RootComponent, {
-  providers: [provideRouter(rootRoutes, ...rootRouterFeatures)],
+  providers: [
+    provideRouter(rootRoutes, ...rootRouterFeatures),
+    { provide: TitleStrategy, useClass: PageTitleStrategyService },
+  ],
 });
