59ac8467723e7f0fb3e6889543f72fc8a43fc099
Peter Jokumsen <pjokumsen@gmail.com>
2023-01-08 19:13:37 +0200

    feat(demo-app): use table in packages page
    #37

--- apps/chill-viking-ng-libs/src/app/pages/packages/packages.component.ts
+++ apps/chill-viking-ng-libs/src/app/pages/packages/packages.component.ts
@@ -4,13 +4,24 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
+import { ChillVikingTableComponent } from '../../features/chill-viking-table/chill-viking-table.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChillVikingTableComponent],
   templateUrl: './packages.component.html',
   styleUrls: ['./packages.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PackagesComponent {}
+export class PackagesComponent {
+  data = [
+    {
+      name: '@chill-viking/layout',
+      description: [
+        'This Angular package provides a layout component for your application',
+        'that keeps its contents contained within the bounds of the web browser.',
+      ].join(' '),
+    },
+  ];
+}
